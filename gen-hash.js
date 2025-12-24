758ad2fc1291baa7b0462ba946061d6343a72981
Haven Technologies <dev@haven-technologies.com>
2025-12-24 12:55:21 -0800

    Security audit: Remove unnecessary documentation, SQL scripts, and sensitive files

--- gen-hash.js
+++
@@ -1,3 +0,0 @@
-const bcrypt = require('bcryptjs');
-const hash = bcrypt.hashSync('password', 12);
-console.log(hash);
--- update-admin.sql
+++
@@ -1,1 +0,0 @@
-UPDATE users SET role='admin', subscription_tier='premium' WHERE email='admin@havenstudy.com';
